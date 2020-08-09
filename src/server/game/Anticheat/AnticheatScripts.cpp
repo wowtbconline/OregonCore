00a97d47dc07c708ced4892514a43eca14ad7ab6
wowtbconline <52467573+wowtbconline@users.noreply.github.com>
2020-08-10 00:00:56 +0300

    Anticheat AC3

---
+++ src/server/game/Anticheat/AnticheatScripts.cpp
@@ -0,0 +1,11 @@
+/* #include "AnticheatScripts.h"
+#include "AnticheatMgr.h"
+AnticheatScripts::AnticheatScripts(): AnticheatScripts("AnticheatScripts") {}
+void AnticheatScripts::OnLogout(Player* player)
+{
+ sAnticheatMgr->HandlePlayerLogout(player);
+}
+void AnticheatScripts::OnLogin(Player* player)
+{
+ sAnticheatMgr->HandlePlayerLogin(player);
+}*/
